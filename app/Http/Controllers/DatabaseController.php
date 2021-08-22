7cde4c3b196e39be07eb4e3b296c7fecf5bb34d2
fajar sidik priatna <jardik.oryza@gmail.com>
2021-08-22 20:17:41 +0700

    add test db

--- app/Http/Controllers/DatabaseController.php
+++ app/Http/Controllers/DatabaseController.php
@@ -3,10 +3,21 @@
 namespace App\Http\Controllers;
 
 // use Illuminate\Http\Request;
+
+use App\Models\User;
 use Illuminate\Support\Facades\Artisan;
 
 class DatabaseController extends Controller
 {
+    public function test()
+    {
+        $user = User::first();
+        return response()->json([
+            'user_id' => $user->id ?? '-',
+            'message' => 'connection ok'
+        ]);
+    }
+    
     public function migrate()
     {
         Artisan::call('migrate');
